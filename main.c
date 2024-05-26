6bb8929ee0755d6686dcc3c0ba603d5f7a184323
Renato A. Silva <97402553+coldrenatinho@users.noreply.github.com>
2024-05-26 19:38:39 -0400

    the file is oppening

--- main.c
+++ main.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // UNEMAT (UNIVERSIDADE DO ESTADOR DO MATO GROSSO) - SINOP
 #define logo "██████╗ ██╗    ██╗ ██████╗ ██████╗ ██████╗         ███████╗██████╗ ██╗██████╗ \n██╔════╝ ██║    ██║██╔═══██╗██╔══██╗██╔══██╗        ██╔════╝██╔══██╗██║██╔══██╗\n██║█████╗██║ █╗ ██║██║   ██║██████╔╝██║  ██║        █████╗  ██║  ██║██║██║  ██║\n██║╚════╝██║███╗██║██║   ██║██╔══██╗██║  ██║        ██╔══╝  ██║  ██║██║██║  ██║\n╚██████╗ ╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝███████╗███████╗██████╔╝██║██████╔╝\n╚═════╝  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚══════╝╚═════╝ ╚═╝╚═════╝\n"
 
-FILE *fptr;
+#define MAX_BUFFER 2000
 
 void readlocalfile()
 {
 
+    FILE *fptr;
     fptr = fopen("test.txt", "r");
+
+    char readBuffer[MAX_BUFFER];
+    char *preadBuffer = &readBuffer;
 
     if (fptr != NULL)
     {
-        printf("File Open Sucessful");
+        printf("File Open Sucessfull\n");
+        while (fgets(readBuffer, MAX_BUFFER, fptr))
+            ;
+        printf("%s", readBuffer);
     }
     else
     {
         printf("File Opne Unsuncessful");
     }
+
+    fclose(fptr);
 }
 
-int main(int argc, char *argv[])
+void writelocalfile()
+{
+    char bufferWrite[MAX_BUFFER];
+
+    FILE *fptr;
+    fptr = fopen("test.txt", "w");
+
+    if (fptr != NULL)
+    {
+        fgets(bufferWrite, MAX_BUFFER, fptr);
+    }
+}
+
+main(int argc, char *argv[])
 {
     readlocalfile();
     return 0;
