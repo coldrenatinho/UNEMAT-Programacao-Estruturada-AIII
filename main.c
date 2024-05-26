21b3c784fa71b2f7d1632137b8c0518e85dade91
Renato A. Silva <97402553+coldrenatinho@users.noreply.github.com>
2024-05-26 18:42:29 -0400

    change branch

--- main.c
+++ main.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//UNEMAT (UNIVERSIDADE DO ESTADOR DO MATO GROSSO) - SINOP
+// UNEMAT (UNIVERSIDADE DO ESTADOR DO MATO GROSSO) - SINOP
 #define logo "██████╗ ██╗    ██╗ ██████╗ ██████╗ ██████╗         ███████╗██████╗ ██╗██████╗ \n██╔════╝ ██║    ██║██╔═══██╗██╔══██╗██╔══██╗        ██╔════╝██╔══██╗██║██╔══██╗\n██║█████╗██║ █╗ ██║██║   ██║██████╔╝██║  ██║        █████╗  ██║  ██║██║██║  ██║\n██║╚════╝██║███╗██║██║   ██║██╔══██╗██║  ██║        ██╔══╝  ██║  ██║██║██║  ██║\n╚██████╗ ╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝███████╗███████╗██████╔╝██║██████╔╝\n╚═════╝  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚══════╝╚═════╝ ╚═╝╚═════╝\n"
-
-
 
 char relativepath[200];
 
-int 
-
-void bufferwite(){
+void bufferwite()
+{
 
     printf("Digite o buffer de entrada:\n");
-    fgets()
+    fgets();
 }
 
-void filew(char input0[400]){
+void filew(char input0[400])
+{
     FILE *fpointer = fopen(input0, "w");
-    if(fpointer == NULL){
+    if (fpointer == NULL)
+    {
         printf("Erro opening file!");
         return 1;
     }
     fprintf(fpointer, )
 }
 
-
-
-
-
-int main(int argc, char *argv[]){
-
-
+int main(int argc, char *argv[])
+{
 }
